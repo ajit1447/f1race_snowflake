db0c932168bc9274a8f7ef27207ac104b725f2cf
ajit1447 <ajitkuls@gmail.com>
2022-10-21 11:15:09 +0530

    Create file_format.tf

---
+++ terraform/snowflake/file_format.tf
@@ -0,0 +1,30 @@
+resource "snowflake_file_format" "csv_file_format" {
+  name        = "csv_file_format"
+  database    = snowflake_schema.f1race_raw.database
+  schema      = snowflake_schema.f1race_raw.name
+  format_type = "CSV"
+  field_delimiter = ","
+  skip_header = 1
+  null_if = ["NULL","null"]
+  empty_field_as_null = TRUE
+  FIELD_OPTIONALLY_ENCLOSED_BY = "\""
+}
+
+resource "snowflake_file_format" "json_file_format" {
+  name        = "json_file_format"
+  database    = snowflake_schema.f1race_raw.database
+  schema      = snowflake_schema.f1race_raw.name
+  format_type = "JSON"
+}
+
+resource "snowflake_file_format" "json_file_format_1" {
+  name        = "json_file_format_1"
+  database    = snowflake_schema.f1race_raw.database
+  schema      = snowflake_schema.f1race_raw.name
+  format_type = "JSON"
+  strip_outer_array = true
+}
+    
+    
+    
+    
