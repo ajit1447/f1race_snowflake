068757818b97b8f192d6857b2892529ba924034e
ajit1447 <ajitkuls@gmail.com>
2022-10-21 16:44:36 +0530

    Update stages.tf

--- terraform/snowflake/stages.tf
+++ terraform/snowflake/stages.tf
@@ -26,7 +26,7 @@ resource "snowflake_stage" "s3_lap_times" {
 }
 
 resource "snowflake_stage" "s3_drivers" {
-  name        = "s3_circuits"
+  name        = "s3_drivers"
   url         = "s3://f1race/raw/drivers.json"
   database    = snowflake_schema.f1race_raw.database
   schema      = snowflake_schema.f1race_raw.name
@@ -35,7 +35,7 @@ resource "snowflake_stage" "s3_drivers" {
 }
 
 resource "snowflake_stage" "s3_pit_stops" {
-  name        = "s3_circuits"
+  name        = "s3_pit_stops"
   url         = "s3://f1race/raw/pit_stops.json"
   database    = snowflake_schema.f1race_raw.database
   schema      = snowflake_schema.f1race_raw.name
@@ -44,7 +44,7 @@ resource "snowflake_stage" "s3_pit_stops" {
 }
 
 resource "snowflake_stage" "s3_constructors" {
-  name        = "s3_circuits"
+  name        = "s3_constructors"
   url         = "s3://f1race/raw/constructors.json"
   database    = snowflake_schema.f1race_raw.database
   schema      = snowflake_schema.f1race_raw.name
@@ -53,7 +53,7 @@ resource "snowflake_stage" "s3_constructors" {
 }
 
 resource "snowflake_stage" "s3_qualifying" {
-  name        = "s3_circuits"
+  name        = "s3_qualifying"
   url         = "s3://f1race/raw/qualifying"
   database    = snowflake_schema.f1race_raw.database
   schema      = snowflake_schema.f1race_raw.name
@@ -62,7 +62,7 @@ resource "snowflake_stage" "s3_qualifying" {
 }
 
 resource "snowflake_stage" "s3_results" {
-  name        = "s3_circuits"
+  name        = "s3_results"
   url         = "s3://f1race/raw/results.json"
   database    = snowflake_schema.f1race_raw.database
   schema      = snowflake_schema.f1race_raw.name
