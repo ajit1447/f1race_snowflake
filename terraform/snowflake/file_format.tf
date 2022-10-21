0266a1003e54135843319daac7a6ee838eff3361
ajit1447 <ajitkuls@gmail.com>
2022-10-21 11:16:16 +0530

    Update file_format.tf

--- terraform/snowflake/file_format.tf
+++ terraform/snowflake/file_format.tf
@@ -6,7 +6,7 @@ resource "snowflake_file_format" "csv_file_format" {
   field_delimiter = ","
   skip_header = 1
   null_if = ["NULL","null"]
-  empty_field_as_null = TRUE
+  empty_field_as_null = true
   FIELD_OPTIONALLY_ENCLOSED_BY = "\""
 }
 
