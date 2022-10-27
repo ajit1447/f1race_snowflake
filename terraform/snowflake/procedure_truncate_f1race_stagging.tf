ed84477abac1477c214dd76c0849e36eef42d87f
ajit1447 <ajitkuls@gmail.com>
2022-10-27 19:31:58 +0530

    Update procedure_truncate_f1race_stagging.tf

--- terraform/snowflake/procedure_truncate_f1race_stagging.tf
+++ terraform/snowflake/procedure_truncate_f1race_stagging.tf
@@ -13,11 +13,9 @@ resource "snowflake_procedure" "proc" {
   return_behavior     = "IMMUTABLE"
   null_input_behavior = "RETURNS NULL ON NULL INPUT"
   statement           = <<EOT
-declare
-    force_delete varchar;
+
     begin
     
-    //  case when p_force=true  then
     
     TRUNCATE TABLE IF EXISTS pit_stops;
     TRUNCATE TABLE IF EXISTS constructors;
@@ -27,21 +25,7 @@ declare
     TRUNCATE TABLE IF EXISTS results;
     TRUNCATE TABLE IF EXISTS qualifying;
     TRUNCATE TABLE IF EXISTS races;
-    force_delete:= ''force true'';
-    // else
-    delete from pit_stops;
-    delete from constructors;
-    delete from circuits;
-    delete from lap_times;
-    delete from drivers;
-    delete from results;
-    delete from qualifying;
-    delete from races;
-    force_delete:= ''force false'';
-    // end;
     
-     return force_delete;
-    
-    end;'
+    end;
 EOT
 }
