a701a46be71a3b91da8b61b420d7fa347c258c4e
ajit1447 <ajitkuls@gmail.com>
2022-10-27 23:20:03 +0530

    Update stages.tf

--- terraform/snowflake/stages.tf
+++ terraform/snowflake/stages.tf
@@ -22,7 +22,7 @@ resource "snowflake_stage" "s3_circuits" {
   database    = snowflake_schema.f1race_raw.database
   schema      = snowflake_schema.f1race_raw.name
   storage_integration  = "S3_F1RACE_RAW"
-  file_format         = " SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '\"' NULL_IF = [NULL, null] "
+  file_format         = " SKIP_HEADER = 1  NULL_IF = ('NULL','null')  FIELD_OPTIONALLY_ENCLOSED_BY = '\"'  "
 }
 
 resource "snowflake_stage" "s3_races" {
