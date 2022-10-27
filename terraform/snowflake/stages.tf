716d8cc9dda7e4c9e51152044feb3aabe6a0e01d
ajit1447 <ajitkuls@gmail.com>
2022-10-27 22:59:19 +0530

    Update stages.tf

--- terraform/snowflake/stages.tf
+++ terraform/snowflake/stages.tf
@@ -1,20 +1,20 @@
-# resource "snowflake_stage" "s3_circuits" {
-#   name        = "S3_CIRCUITS"
-#   url         = "s3://f1race/raw/circuits.csv"
-#   database    = snowflake_schema.f1race_raw.database
-#   schema      = snowflake_schema.f1race_raw.name
-#   storage_integration  = "S3_F1RACE_RAW"
-#   file_format         = "FIELD_DELIMITER = |"
-# }
-
 resource "snowflake_stage" "s3_circuits" {
   name        = "S3_CIRCUITS"
   url         = "s3://f1race/raw/circuits.csv"
   database    = snowflake_schema.f1race_raw.database
   schema      = snowflake_schema.f1race_raw.name
   storage_integration  = "S3_F1RACE_RAW"
-  file_format         = " SKIP_HEADER = 1  NULL_IF = ('NULL','null')  FIELD_OPTIONALLY_ENCLOSED_BY = '\"' "
+  file_format         = snowflake_file_format.csv_file_format.name
 }
+
+# resource "snowflake_stage" "s3_circuits" {
+#   name        = "S3_CIRCUITS"
+#   url         = "s3://f1race/raw/circuits.csv"
+#   database    = snowflake_schema.f1race_raw.database
+#   schema      = snowflake_schema.f1race_raw.name
+#   storage_integration  = "S3_F1RACE_RAW"
+#   file_format         = " SKIP_HEADER = 1  NULL_IF = ('NULL','null')  FIELD_OPTIONALLY_ENCLOSED_BY = '\"' "
+# }
 
 # resource "snowflake_stage" "s3_races" {
 #   name        = "s3_races"
