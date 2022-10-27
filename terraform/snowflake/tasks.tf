20869292f3dc0b7f57d4b6288041678f3df64ffe
ajit1447 <ajitkuls@gmail.com>
2022-10-27 23:41:22 +0530

    Update tasks.tf

--- terraform/snowflake/tasks.tf
+++ terraform/snowflake/tasks.tf
@@ -4,7 +4,7 @@ resource snowflake_task task_stagging_truncate {
   database  = snowflake_schema.f1race_raw.database
   schema    = snowflake_schema.f1race_raw.name
   warehouse = "COMPUTE_WH"
-
+  schedule      = "360 MINUTE"
   name          = "task_stagging_truncate"
   sql_statement = "call truncate_f1race_stagging(p_force=>true);"
 
